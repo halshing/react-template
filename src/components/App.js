bb6b1db6402c4219e67aba2892b9019decd500e4
Hasan <hasan.alshingieti@gmail.com>
2020-05-25 20:46:57 -0400

    app created

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/App.scss";
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1>React Template</h1>
+    </div>
+  );
+};
+
+export default App;
